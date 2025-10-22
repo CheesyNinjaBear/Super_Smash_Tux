ce123f9464956ceb6c1b1aaf2c2e10ea5f6188d9
DianaΣ:3 <DianaGodessOfTheHunt@proton.me>
2025-10-22 10:21:17 -0400

    worked on trying to get the player model to turn in the direction of movemebt did not work yet.Σ:3

--- super_smash_tux/player.gd
+++ super_smash_tux/player.gd
@@ -14,15 +14,28 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+
+	# rotate the player when you move
+	#if Input.is_action_just_pressed("ui_right"):
+	#	self.look_at(Vector3(-1,0,0), Vector3(1,0,0))
+		
+	#if Input.is_action_just_pressed("ui_left"):
+	#	self.rotate_y(180)
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# As good practice, you should replace UI actions with custom gameplay actions.		
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		#velocity.z = direction.z * SPEED # causes the player to go forward/backward
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	#if velocity.x != 0:
+	#	self.look_at(Vector3(input_dir.x, 0, 0))
+
+
 
 	move_and_slide()
